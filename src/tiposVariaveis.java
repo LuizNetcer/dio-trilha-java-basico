f9160d946f678a2a91dfb2fe3123d2faa3352bf9
LuizNetcer <luizhenrique_netcer@hotmail.com>
2023-06-16 16:44:07 -0300

    criando projeto para exemplo de tipos e variaveis

---
+++ src/tiposVariaveis.java
@@ -0,0 +1,4 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class tiposVariaveis {
+    public static void main(String[] args) {
