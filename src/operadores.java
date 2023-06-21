4c7b3cc69a8a4822499d143195e14ba5685b312b
LuizNetcer <luizhenrique_netcer@hotmail.com>
2023-06-21 15:43:52 -0300

    criando projeto para exemplo de operadores 2

--- src/operadores.java
+++ src/operadores.java
@@ -18,6 +18,26 @@ public class operadores {
 
         concatenacao = "1"+(1+1+1);
         System.out.println(concatenacao);
+          /*incremento*/
+        int numero = 5;
+        numero++;
+        System.out.println(numero);
+
+        /*operador ternario*/
+        int a,b;
+        a=5;
+        b=6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+
+        /*operador relacional*/
+        int numero1 = 1;
+        int numero2 =2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println(simNao);
 
     }
 }
